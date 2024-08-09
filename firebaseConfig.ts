febc28490a12676aa57411e27ddf4ad2416da380
LuizFigueiredo <figueiredocabralluizcarlos@gmail.com>
2024-08-09 15:25:53 -0300

    fix

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: VITE_YOUR_API_KEY,
-  authDomain: VITE_YOUR_AUTH_DOMAIN,
-  projectId: VITE_YOUR_PROJECT_ID,
-  storageBucket: VITE_YOUR_STORAGE_BUCKET,
-  messagingSenderId: VITE_YOUR_MESSAGING_SENDER_ID,
-  appId: VITE_YOUR_APP_ID,
-  measurementId: VITE_YOUR_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_YOUR_API_KEY,
+  authDomain: import.meta.env.VITE_YOUR_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_YOUR_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_YOUR_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_YOUR_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_YOUR_APP_ID,
+  measurementId: import.meta.env.VITE_YOUR_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
